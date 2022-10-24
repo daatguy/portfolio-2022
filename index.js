f2e67f7a2f4cd3a520c1df8be4d9d01c7c543b6e
daatguy <daatguy@outlook.com>
2022-10-24 08:41:21 -0400

    Fixed production deployment client side setup

--- index.js
+++ index.js
@@ -25,5 +25,15 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/commentRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve static assets in production
+  app.use(express.static('client/build'));
+  // Serve index.html for unrecognized routes
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
