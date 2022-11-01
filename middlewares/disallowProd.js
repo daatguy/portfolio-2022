1ac6cf41c0de2bab418beeb5afcbae1a78754786
daatguy <daatguy@outlook.com>
2022-11-01 01:29:52 -0400

    Comment prompt margin fix and temporary user reset api

--- middlewares/disallowProd.js
+++ middlewares/disallowProd.js
@@ -1,8 +1,7 @@
 module.exports = (req, res, next) => {
   // This endpoint doesn't exist... shhh...
-  console.log(process.env.NODE_ENV);
-  if(process.env.NODE_ENV === 'production') {
+  /*if(process.env.NODE_ENV === 'production') {
     return res.status(403).send({error: 'Development only endpoint'});
-  }
+  }*/
   next();
 }
